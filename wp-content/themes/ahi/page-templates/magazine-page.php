fac421bbea4aa78f6c40b28bc69ba1156def8c7c
robalford <robertsalford@gmail.com>
2015-12-22 10:21:40 -0800

    refactoring custom page templates, still working on magazine and book templates.

--- wp-content/themes/ahi/page-templates/magazine-page.php
+++ wp-content/themes/ahi/page-templates/magazine-page.php
@@ -16,11 +16,11 @@ $press_pages = new WP_Query( 'post_type=page&meta_value=Magazine&order=DESC&orde
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		
+
 		<?php while ( $press_pages->have_posts() ) : $press_pages->the_post(); ?>
-			
+
 			<?php get_template_part( 'template-parts/content', 'page' ); ?>
-			
+
 			<?php
 				if (has_post_thumbnail()) {
 					echo '<div class="single-post-thumbnail magazine-thumbnail">'; // added class for custom styling in recognition.css
@@ -32,11 +32,11 @@ $press_pages = new WP_Query( 'post_type=page&meta_value=Magazine&order=DESC&orde
 					echo '</div>';
 				}
 			?>
-			
+
 		<?php endwhile; // End of the loop. ?>
-		
+
 	</main><!-- #main -->
-	
+
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
