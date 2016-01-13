65e91b1b348d058bfd395423d18257ea82e39854
robalford <robertsalford@gmail.com>
2016-01-13 15:07:35 -0800

    refactored footer.php and about-page.php for better escaping and html/php structure

--- wp-content/themes/ahi/footer.php
+++ wp-content/themes/ahi/footer.php
@@ -16,7 +16,7 @@
 			<!-- commented out underscores footer text. and replaced with custom text.
 			<a href="<?php //echo esc_url( __( 'https://wordpress.org/', 'ahi' ) ); ?>"><?php //printf( esc_html__( 'Proudly powered by %s', 'ahi' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>-->
-			<p><?php printf( esc_html__( 'contact@alfordhomesinc.com | 360.779.7268 | &copy; 2014 Alford Homes Inc. All rights reserved.' ) ); ?></p>
+			<p><?php echo esc_html__( 'contact@alfordhomesinc.com | 360.779.7268 | &copy; 2014 Alford Homes Inc. All rights reserved.' ); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- wp-content/themes/ahi/page-templates/about-page.php
+++ wp-content/themes/ahi/page-templates/about-page.php
@@ -8,13 +8,13 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php
-				if (has_post_thumbnail()) {
-					echo '<div class="single-post-thumbnail">';
-					echo the_post_thumbnail('about-page');
-					echo '</div>';
-				}
-			?>
+			<div class="single-post-thumbnail">
+				<?php
+					if (has_post_thumbnail()) {
+						echo the_post_thumbnail('about-page');
+					}
+				?>
+			</div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
