b3d4b2c326811d61bb92256973e3b6636119cb0b
robalford <robertsalford@gmail.com>
2016-01-14 09:09:07 -0800

    escaped urls for books and magazine pages

--- wp-content/themes/ahi/page-templates/magazine-page.php
+++ wp-content/themes/ahi/page-templates/magazine-page.php
@@ -19,7 +19,7 @@ $press_pages = new WP_Query( 'post_type=page&meta_value=Magazine&order=DESC&orde
 
 		<?php while ( $press_pages->have_posts() ) : $press_pages->the_post(); ?>
 			<div class="single-post-thumbnail magazine-thumbnail">
-				<a href="<?php echo get_post_meta( get_the_ID(), 'Press URL', true ); ?>" target="_blank">
+				<a href="<?php echo esc_url( get_post_meta( get_the_ID(), 'Press URL', true ) ); ?>" target="_blank">
 					<?php
 						if (has_post_thumbnail()) {
 							echo the_post_thumbnail('press-page');
